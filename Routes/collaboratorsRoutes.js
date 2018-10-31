ca758e321bf38a6e7eb05e9aeb68c3139ab2e2d9
marshall <marshall@marshalls-MacBook-Air.local>
2018-10-31 15:08:46 -0700

    post to collaborators working

--- Routes/collaboratorsRoutes.js
+++ Routes/collaboratorsRoutes.js
@@ -16,6 +16,18 @@ router.get('', (req, res) => {
 		})
 })
 
+router.post('', (req, res) => {
+	const {name} = req.body
+	db('collaborators').insert({name}).into('collaborators')
+		.then(response => {
+			res.status(201).json(response)
+		})
+		.catch(error => {
+			console.log(error)
+			res.status(500).json({msg: 'there was an error creating collaborator'})
+		})
+})
+
 
 
 
